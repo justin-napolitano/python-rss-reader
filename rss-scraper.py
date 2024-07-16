d29bc5d30fcd84811de62f7cf021ad71e3770bc0
Cobra <cobra.jnapolitano@gmail.com>
2024-07-16 15:57:52 -0500

    cloud-logging

--- rss-scraper.py
+++ rss-scraper.py
@@ -1,101 +1,147 @@
 import feedparser
-
 from datetime import datetime
-import time
+import requests
+import json
+import argparse
+import os
+from gcputils.GoogleCloudLogging import GoogleCloudLogging
+import logging
+from dotenv import load_dotenv
 
-
-# convert to integer
-def get_last_run():
-    try:
-        with open("last_run.txt", "r") as fp:
-            epoch = fp.read()
-
-        epoch = int(epoch)
-        return epoch
-    except Exception as e:
-        raise e
-
-def get_news_feed(feed_url):
+def get_news_feed(feed_url="https://jnapolitano.com/index.xml"):
     try:
         NewsFeed = feedparser.parse(feed_url)
         return NewsFeed
     except Exception as e:
-        raise e
+        logging.error(f"An error occurred while fetching the news feed: {e}")
+        return None
 
 
-def convert_to_epoch(given):
-
-    try: 
-        # The given date string
-        date_str = given
-
-        # Define the format of the date string
-        date_format = "%a, %d %b %Y %H:%M:%S %z"
-
-        # Convert the date string to a datetime object
-        dt = datetime.strptime(date_str, date_format)
-
-        # Convert the datetime object to a timestamp (epoch time)
-        epoch_time = int(dt.timestamp())
-
-        return epoch_time
-    except Exception as e:
-        raise e
-
-def write_current_dt_to_file(file_name = "last_run.txt"):
-
+def format_datetime(date_str, date_format="%a, %d %b %Y %H:%M:%S %z"):
     try:
-        # Get the current epoch time
-        current_epoch_time = int(time.time())
-
-        # Write the current epoch time to the file
-        with open(file_name, "w") as file:
-            file.write(str(current_epoch_time))
-        return True
+        dt = datetime.strptime(date_str, date_format)
+        formatted_date = dt.strftime("%Y-%m-%dT%H:%M:%S")
+        return formatted_date
     except Exception as e:
-        raise e
+        logging.error(f"An error occurred while formatting date: {e}")
+        return None
 
 
+def update_database_feed(entry, base_url):
+    try:
+        url = f"{base_url}/update/feed"
+        headers = {'Content-Type': 'application/json'}
+        formatted_pub_date = format_datetime(entry.get('published'))
+        data = {
+            "title": entry.get('title'),
+            "link": entry.get('link'),
+            "pubDate": formatted_pub_date,
+            "guid": entry.get('id'),
+            "description": entry.get('summary')
+        }
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+        if response.status_code == 201:
+            logging.info(f"Successfully added entry: {data['title']}")
+        elif response.status_code == 200:
+            logging.info(f"Entry updated or no update needed for: {data['title']}")
+        else:
+            logging.error(f"Failed to update database for entry: {data['title']}, Status Code: {response.status_code}, Message: {response.text}")
+    except Exception as e:
+        logging.error(f"An error occurred while updating the database: {e}")
 
-try:
-    # keys_i_want = ['author_name','author_email', 'author_id','postid','id','published','title']
-    
-    out_struct = {
-        "author_name": None,
-        "author_email": None,
-        "author_id": None,
-        "postid": None,
-        "published": None,
-        "title": None
-    }
 
-    last_run_epoch = get_last_run()
-    NewsFeed = get_news_feed("https://jnapolitano.com/en/posts/index.xml")
-   
-    for post in NewsFeed.entries:
+def update_database_build(entry, base_url):
+    try:
+        url = f"{base_url}/update/builds"
+        headers = {'Content-Type': 'application/json'}
+        formatted_last_build_date = format_datetime(entry.get('lastBuildDate'))
+        data = {
+            "title": entry.get('title'),
+            "link": entry.get('link'),
+            "description": entry.get('description'),
+            "generator": entry.get('generator'),
+            "language": entry.get('language'),
+            "copyright": entry.get('copyright'),
+            "lastBuildDate": formatted_last_build_date,
+            "atom_link_href": entry.get('atom_link_href'),
+            "atom_link_rel": entry.get('atom_link_rel'),
+            "atom_link_type": entry.get('atom_link_type')
+        }
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+        if response.status_code == 201:
+            logging.info(f"Successfully added build: {data['title']}")
+        elif response.status_code == 200:
+            logging.info(f"Build updated or no update needed for: {data['title']}")
+        else:
+            logging.error(f"Failed to update database for build: {data['title']}, Status Code: {response.status_code}, Message: {response.text}")
+    except Exception as e:
+        logging.error(f"An error occurred while updating the database: {e}")
 
-        try: 
-            out_struct['author_name'] = post['author_name']
-            out_struct['author_email'] = post['author_email']
-            out_struct['author_id'] = post['author_id']
-            out_struct['postid'] = post['postid']
-            out_struct['published'] = convert_to_epoch(post['published'])
-            out_struct['title'] = post['title']
-        except Exception as e:
-            raise e
+
+def process_feed(base_url):
+    try:
+        NewsFeed = get_news_feed()
         
-        print(out_struct)
+        if not NewsFeed:
+            logging.error("Failed to retrieve news feed.")
+            return
 
-        ## Write a script to select the given post id
-        ## if exists do nothing? Or update. IDK 
-        ## if not exists then insert record
-        # or drop into an if null.. 
-        #somethin glike
-        #SELECT IFNULL( (SELECT field1 FROM table WHERE id = 123 LIMIT 1) ,'not found');
-        # I'll have to experiment to figure it ou
-    # run the write epoch to file function
-    result = write_current_dt_to_file()
+        for entry in NewsFeed.entries:
+            # Unpacking dictionary keys to variables
+            title = entry.get('title')
+            title_detail = entry.get('title_detail')
+            links = entry.get('links')
+            link = entry.get('link')
+            published = entry.get('published')
+            published_parsed = entry.get('published_parsed')
+            entry_id = entry.get('id')
+            guidislink = entry.get('guidislink')
+            summary = entry.get('summary')
+            summary_detail = entry.get('summary_detail')
+            # description = entry.get('description')
+            # generator = entry.get('generator')
+            # language = entry.get('language')
+            # copyright = entry.get('copyright')
+            # last_build_date = entry.get('lastBuildDate')
+            # atom_link_href = entry.get('atom_link_href')
+            # atom_link_rel = entry.get('atom_link_rel')
+            # atom_link_type = entry.get('atom_link_type')
+
+            # Logging for debugging
+            logging.info(f"Title: {title}")
+            logging.info(f"Title Detail: {title_detail}")
+            logging.info(f"Links: {links}")
+            logging.info(f"Link: {link}")
+            logging.info(f"Published: {published}")
+            logging.info(f"Published Parsed: {published_parsed}")
+            logging.info(f"ID: {entry_id}")
+            logging.info(f"GUID is Link: {guidislink}")
+            logging.info(f"Summary: {summary}")
+            logging.info(f"Summary Detail: {summary_detail}")
+
+            if published:
+                logging.info("New entry found:", title)
+                update_database_feed(entry, base_url)
+
+    except Exception as e:
+        logging.error(f"An error occurred during the process: {e}")
 
 
-except Exception as e :
-    raise e
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process RSS feed updates.')
+    parser.add_argument('--url', type=str, default="https://rss-updater-pkpovjepjq-wl.a.run.app",
+                        help='Base URL for the API endpoint')
+    parser.add_argument('--local', action='store_true', help='Use local credentials for Google Cloud Logging')
+    args = parser.parse_args()
+
+    # Load environment variables from .env file
+    load_dotenv()
+    # Setup Google Cloud Logging
+    project_id = os.environ.get("PROJECT_NAME")
+    credentials_path = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS") if args.local else None
+    print(project_id)
+    gcl = GoogleCloudLogging(project_id, credentials_path)
+    gcl.setup_logging()
+
+    # Run the feed processing with the provided URL or default
+    process_feed(args.url)
